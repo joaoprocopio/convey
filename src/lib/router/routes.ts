7f4c1a0ff6d48f0142b3c43ace29d033132221e4
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-06-18 01:13:30 -0300

    vai

--- src/lib/router/routes.ts
+++ src/lib/router/routes.ts
@@ -1,3 +1,4 @@
+import { h } from "vue";
 import type { RouteRecordRaw } from "vue-router";
 
 import DefaultLayout from "~/layouts/default-layout";
@@ -27,25 +28,25 @@ export const routes: RouteRecordRaw[] = [
   {
     path: "/produtos",
     name: ProdutosPageName,
-    component: () => import("~/pages/deteccoes-page"),
+    component: () => h("div", null, [h("h1", null, "produtos")]),
     meta: { layout: DefaultLayout },
   },
   {
     path: "/produtos/exemplo-1",
     name: ProdutosExemplo1PageName,
-    component: () => import("~/pages/deteccoes-page"),
+    component: () => h("div", null, [h("h1", null, "exemplo 1")]),
     meta: { layout: DefaultLayout },
   },
   {
     path: "/produtos/exemplo-2",
     name: ProdutosExemplo2PageName,
-    component: () => import("~/pages/deteccoes-page"),
+    component: () => h("div", null, [h("h1", null, "exemplo 2")]),
     meta: { layout: DefaultLayout },
   },
   {
     path: "/produtos/exemplo-3",
     name: ProdutosExemplo3PageName,
-    component: () => import("~/pages/deteccoes-page"),
+    component: () => h("div", null, [h("h1", null, "exemplo 3")]),
     meta: { layout: DefaultLayout },
   },
 ];
