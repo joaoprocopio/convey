9ccf0e288db2f17e18e511f7e787ae692d6c916f
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-06-18 01:14:57 -0300

    exemplos

--- src/lib/router/routes.ts
+++ src/lib/router/routes.ts
@@ -28,25 +28,41 @@ export const routes: RouteRecordRaw[] = [
   {
     path: "/produtos",
     name: ProdutosPageName,
-    component: () => h("div", null, [h("h1", null, "produtos")]),
+    component: {
+      setup() {
+        return () => h("div", null, [h("h1", null, "produtos")]);
+      },
+    },
     meta: { layout: DefaultLayout },
   },
   {
     path: "/produtos/exemplo-1",
     name: ProdutosExemplo1PageName,
-    component: () => h("div", null, [h("h1", null, "exemplo 1")]),
+    component: {
+      setup() {
+        return () => h("div", null, [h("h1", null, "exemplo 1")]);
+      },
+    },
     meta: { layout: DefaultLayout },
   },
   {
     path: "/produtos/exemplo-2",
     name: ProdutosExemplo2PageName,
-    component: () => h("div", null, [h("h1", null, "exemplo 2")]),
+    component: {
+      setup() {
+        return () => h("div", null, [h("h1", null, "exemplo 2")]);
+      },
+    },
     meta: { layout: DefaultLayout },
   },
   {
     path: "/produtos/exemplo-3",
     name: ProdutosExemplo3PageName,
-    component: () => h("div", null, [h("h1", null, "exemplo 3")]),
+    component: {
+      setup() {
+        return () => h("div", null, [h("h1", null, "exemplo 3")]);
+      },
+    },
     meta: { layout: DefaultLayout },
   },
 ];
