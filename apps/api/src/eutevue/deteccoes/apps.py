ba35a2ab6f9e24cc41b1e514e625ca5e9dd07878
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-06-28 15:17:02 -0300

    lesgo

---
+++ apps/api/src/eutevue/deteccoes/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DeteccoesConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "eutevue.deteccoes"
--- apps/api/src/eutevue/settings.py
+++ apps/api/src/eutevue/settings.py
@@ -30,13 +30,18 @@ ALLOWED_HOSTS: list[str] = []
 
 # Application definition
 
+
 INSTALLED_APPS = [
+    # Django Apps
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Local Apps
+    "eutevue.deteccoes",
+    # Third-party apps
 ]
 
 MIDDLEWARE = [
