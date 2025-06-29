b4b91391b0dff22996af47938d2aca78eb581419
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-06-29 02:34:24 -0300

    routes

--- apps/www/src/lib/router/routes.ts
+++ apps/www/src/lib/router/routes.ts
@@ -1,3 +1,4 @@
+import { h } from "vue";
 import type { RouteRecordRaw } from "vue-router";
 
 import DefaultLayout from "~/layouts/default-layout";
@@ -7,14 +8,34 @@ import { DeteccaoPageName, DeteccoesPageName } from "./constants";
 export const routes: RouteRecordRaw[] = [
   {
     path: "/",
+    redirect: {
+      name: DeteccoesPageName,
+    },
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    redirect: {
+      name: DeteccoesPageName,
+    },
+  },
+  {
+    path: "/shows",
     name: DeteccoesPageName,
     component: () => import("~/pages/deteccoes-page"),
     meta: { layout: DefaultLayout },
   },
   {
-    path: "/:id",
+    path: "/shows/:id",
     name: DeteccaoPageName,
     component: () => import("~/pages/deteccao-page"),
     meta: { layout: DefaultLayout },
+  },
+  {
+    path: "/atracoes",
+    component: {
+      setup() {
+        return () => h("div", null, [h("h1", null, "exemplo 1")]);
+      },
+    },
   },
 ];
