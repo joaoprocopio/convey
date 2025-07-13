7e50e31bdbac41d862e632be9f3606e518709252
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-07-13 12:46:03 -0300

    vai

--- apps/www/src/lib/theme/plugin.ts
+++ apps/www/src/lib/theme/plugin.ts
@@ -15,13 +15,9 @@ export const theme: FunctionPlugin = async (app) => {
   const theme = ref<TTHeme>(Theme.System);
   const media = window.matchMedia(PrefersDarkColorScheme);
 
-  watch(
-    theme,
-    (nextTheme) => {
-      applyTheme(nextTheme, media);
-    },
-    { immediate: true },
-  );
+  watch(theme, (nextTheme) => applyTheme(nextTheme, media), {
+    immediate: true,
+  });
 
   media.addEventListener("change", () => applyTheme(theme.value, media));
 
--- apps/www/vite.config.js
+++ apps/www/vite.config.js
@@ -2,12 +2,18 @@ import { fileURLToPath, URL } from "node:url";
 
 import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
+// import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig(() => ({
-  plugins: [tailwindcss(), vue(), vueDevTools()],
+  plugins: [
+    // visualizer(),
+    tailwindcss(),
+    vue(),
+    vueDevTools(),
+  ],
   publicDir: "./src/public",
   resolve: {
     alias: {
