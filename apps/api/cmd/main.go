7a9db910cc6c7feccf1a7e0b29efd651ee81e5f6
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-07-20 18:31:13 -0300

    errgrp

--- apps/api/cmd/main.go
+++ apps/api/cmd/main.go
@@ -26,45 +26,37 @@ func main() {
 func run(ctx context.Context, logger *slog.Logger) error {
 	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt)
 	defer cancel()
-	g, ctx := errgroup.WithContext(ctx)
+	grp, ctx := errgroup.WithContext(ctx)
 
 	srv := server.NewServer(server.NewConfig(), logger)
 
-	g.Go(func() error {
-		return startup(srv, logger)
+	grp.Go(func() error {
+		logger.Info("main: server is listening", slog.String("address", fmt.Sprintf("http://%s", srv.Addr)))
+
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			return err
+		}
+
+		return nil
 	})
 
-	g.Go(func() error {
-		return shutdown(ctx, srv, logger)
-	})
+	grp.Go(func() error {
+		<-ctx.Done()
 
-	if err := g.Wait(); err != nil {
-		return err
-	}
+		ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+		defer cancel()
 
-	return nil
-}
-
-func startup(srv *http.Server, logger *slog.Logger) error {
-	logger.Info("main: server is listening", slog.String("address", fmt.Sprintf("http://%s", srv.Addr)))
-
-	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		return err
-	}
-
-	return nil
-}
-
-func shutdown(ctx context.Context, srv *http.Server, logger *slog.Logger) error {
-	<-ctx.Done()
-
-	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
-	defer cancel()
-
-	if err := srv.Shutdown(ctx); err != nil {
-		return err
-	} else {
 		logger.Info("main: gracefully shutting down http server")
+
+		if err := srv.Shutdown(ctx); err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	if err := grp.Wait(); err != nil {
+		return err
 	}
 
 	return nil
--- apps/api/go.mod
+++ apps/api/go.mod
@@ -2,4 +2,4 @@ module convey
 
 go 1.24.5
 
-require golang.org/x/sync v0.16.0 // indirect
+require golang.org/x/sync v0.16.0
