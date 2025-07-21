43cca272adae857be85bb7225ae37625aafbe9d7
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-07-21 19:09:54 -0300

    vai

---
+++ apps/api/internal/db/migrations/00001_init.sql
@@ -0,0 +1,33 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(254) UNIQUE NOT NULL
+);
+
+CREATE TYPE proposta_status AS ENUM('backlog');
+
+CREATE TABLE propostas (
+    id SERIAL PRIMARY KEY,
+    status proposta_status NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    assignee_id INTEGER NULL,
+
+    FOREIGN KEY (assignee_id) REFERENCES users (id) ON DELETE SET NULL
+);
+
+CREATE TABLE proposta_attachments (
+    id SERIAL PRIMARY KEY,
+    proposta_id INTEGER NOT NULL,
+
+    FOREIGN KEY (proposta_id) REFERENCES propostas (id) ON DELETE CASCADE
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE proposta_attachments;
+DROP TABLE propostas;
+DROP TABLE users;
+DROP TYPE proposta_status;
+-- +goose StatementEnd
