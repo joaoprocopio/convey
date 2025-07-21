c4d77c182e8722e5929f77104eb00da75bca2d59
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-07-20 23:43:33 -0300

    propostas

--- apps/api/internal/propostas/handler.go
+++ apps/api/internal/propostas/handler.go
@@ -6,13 +6,40 @@ import (
 )
 
 type Proposta struct {
-	ID int `json:"id"`
+	ID          int                  `json:"id"`
+	Status      string               `json:"status"`
+	Assignee    string               `json:"assignee"`
+	Name        string               `json:"name"`
+	Attachments []PropostaAttachment `json:"attachments"`
+}
+
+type PropostaAttachment struct {
+	ContentType string `json:"content_type"`
+	Filename    string `json:"filename"`
+	URL         string `json:"url"`
 }
 
 var HandleListPropostas http.HandlerFunc = http.HandlerFunc(handleListPropostas)
 
 func handleListPropostas(w http.ResponseWriter, r *http.Request) {
-	codec.WriteEncodedJSON(w, r, http.StatusOK, Proposta{
-		ID: 1,
+	codec.WriteEncodedJSON(w, r, http.StatusOK, []Proposta{
+		{
+			ID:       1,
+			Status:   "backlog",
+			Assignee: "pedro@embalae.com",
+			Name:     "Proposta de Embalagem",
+			Attachments: []PropostaAttachment{
+				{
+					ContentType: "application/pdf",
+					Filename:    "proposta.pdf",
+					URL:         "https://example.com/proposta.pdf",
+				},
+				{
+					ContentType: "image/png",
+					Filename:    "logo.png",
+					URL:         "https://example.com/logo.png",
+				},
+			},
+		},
 	})
 }
