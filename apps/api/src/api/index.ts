08e3d45afcc9966a9322b97378c3f220c51e1d69
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-07-23 00:55:44 -0300

    vai

--- apps/api/src/api/index.ts
+++ apps/api/src/api/index.ts
@@ -1,5 +1,7 @@
 import { defineEventHandler } from 'h3'
 
 export default defineEventHandler(() => {
-  return 'Start by editing <code>server/routes/index.ts</code>.'
+  return {
+    hello: 'world',
+  }
 })
