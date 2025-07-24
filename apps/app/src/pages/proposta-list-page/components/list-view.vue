34dff88c6f940235133133dea81d40c842b13eb8
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-07-24 03:43:25 -0300

    vai

--- apps/app/src/pages/proposta-list-page/components/list-view.vue
+++ apps/app/src/pages/proposta-list-page/components/list-view.vue
@@ -2,6 +2,7 @@
 import { useQuery } from '@tanstack/vue-query'
 import { ChevronDown } from 'lucide-vue-next'
 
+import { Checkbox } from '~/lib/shadcn/ui/checkbox'
 import {
   Collapsible,
   CollapsibleContent,
@@ -9,6 +10,7 @@ import {
 } from '~/lib/shadcn/ui/collapsible'
 import { propostaQueries } from '~/queries/proposta'
 import { PropostaStatus } from '~/schemas/proposta'
+import { isEmpty } from '~/utils/is'
 
 const propostas = useQuery(propostaQueries.all())
 </script>
@@ -17,34 +19,37 @@ const propostas = useQuery(propostaQueries.all())
   <div v-if="propostas.isSuccess.value">
     <Collapsible
       default-open
-      v-slot="{ open }"
+      class="group"
       v-for="status in PropostaStatus"
       :key="status.value"
     >
-      <CollapsibleTrigger as-child>
-        <div
-          :data-open="open"
-          class="group sticky top-24 z-10 flex h-subheader items-center gap-4 bg-secondary/70 px-6 text-muted-foreground backdrop-blur-xl"
+      <template v-if="!isEmpty(propostas.data.value![status.value])">
+        <CollapsibleTrigger
+          class="sticky top-24 z-10 flex h-subheader w-full items-center gap-4 bg-secondary/70 px-6 text-muted-foreground backdrop-blur-xl"
         >
           <ChevronDown
-            class="size-3.5 transition duration-300 group-data-[open=true]:rotate-180"
+            class="size-3.5 transition duration-300 group-data-[state=closed]:rotate-180"
           />
           <component :is="status.icon" class="size-5" />
           <p class="text-sm text-foreground">{{ status.title }}</p>
           <p class="text-xs">
             {{ propostas.data.value![status.value]?.length }}
           </p>
-        </div>
-      </CollapsibleTrigger>
+        </CollapsibleTrigger>
 
-      <CollapsibleContent>
-        <div
-          v-for="proposta in propostas.data.value![status.value]"
-          :key="proposta.id"
-        >
-          {{ proposta }}
-        </div>
-      </CollapsibleContent>
+        <CollapsibleContent>
+          <div
+            v-for="proposta in propostas.data.value![status.value]"
+            class="flex px-6 py-2"
+            :key="proposta.id"
+          >
+            <Checkbox class="size-5 [&_svg]:size-3.5" />
+            <component :is="status.icon" />
+            <span>{{ proposta.assignee?.email }}</span>
+            <span>{{ proposta.name }}</span>
+          </div>
+        </CollapsibleContent>
+      </template>
     </Collapsible>
   </div>
 </template>
--- apps/app/src/utils/is.ts
+++ apps/app/src/utils/is.ts
@@ -1,5 +1,35 @@
 import type { Component } from 'vue'
 
+export function isEmpty(value: unknown): boolean {
+  if (isNil(value)) {
+    return true
+  }
+
+  if (isArray(value) || isString(value)) {
+    return !value.length
+  }
+
+  if (value instanceof Map || value instanceof Set) {
+    return !value.size
+  }
+
+  for (const key in value as object) {
+    if (Object.prototype.hasOwnProperty.call(value, key)) {
+      return false
+    }
+  }
+
+  return true
+}
+
+export function isArray<T>(array: unknown): array is Array<T> {
+  return Array.isArray(array)
+}
+
+export function isString(value: unknown): value is string {
+  return typeof value === 'string' || value instanceof String
+}
+
 export function isComp(value: unknown): value is Component {
   return (
     !isNil(value) &&
