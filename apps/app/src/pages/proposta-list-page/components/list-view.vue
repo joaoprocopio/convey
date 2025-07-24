63bc5eb8295f16a777712b3a88b857d388ac1365
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-07-24 03:30:32 -0300

    chevron

--- apps/app/src/pages/proposta-list-page/components/list-view.vue
+++ apps/app/src/pages/proposta-list-page/components/list-view.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { useQuery } from '@tanstack/vue-query'
+import { ChevronDown } from 'lucide-vue-next'
 
 import {
   Collapsible,
@@ -16,13 +17,18 @@ const propostas = useQuery(propostaQueries.all())
   <div v-if="propostas.isSuccess.value">
     <Collapsible
       default-open
+      v-slot="{ open }"
       v-for="status in PropostaStatus"
       :key="status.value"
     >
       <CollapsibleTrigger as-child>
         <div
-          class="sticky top-24 z-10 flex h-subheader items-center gap-4 bg-secondary/70 px-6 text-muted-foreground backdrop-blur-xl"
+          :data-open="open"
+          class="group sticky top-24 z-10 flex h-subheader items-center gap-4 bg-secondary/70 px-6 text-muted-foreground backdrop-blur-xl"
         >
+          <ChevronDown
+            class="size-3.5 transition duration-300 group-data-[open=true]:rotate-180"
+          />
           <component :is="status.icon" class="size-5" />
           <p class="text-sm text-foreground">{{ status.title }}</p>
           <p class="text-xs">
