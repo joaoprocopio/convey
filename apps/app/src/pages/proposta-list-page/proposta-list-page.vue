7863c99a684a0b30eca9f7b4b250b285a7fe1330
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-07-24 02:32:13 -0300

    vai

--- apps/app/src/pages/proposta-list-page/proposta-list-page.vue
+++ apps/app/src/pages/proposta-list-page/proposta-list-page.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import { useQuery } from '@tanstack/vue-query'
-import { ListCheck, ListRestart } from 'lucide-vue-next'
+import { ListCheck, ListRestart, Search } from 'lucide-vue-next'
 
+import { Input } from '~/lib/shadcn/ui/input'
 import { SidebarTrigger, useSidebar } from '~/lib/shadcn/ui/sidebar'
 import { propostaQueries } from '~/queries/proposta'
 
@@ -9,29 +10,36 @@ const { open } = useSidebar()
 
 const propostas = useQuery({
   ...propostaQueries.all(),
-  refetchInterval: 5 * 1000,
+  refetchInterval: 15 * 1000,
 })
 </script>
 
 <template>
   <Teleport to="#sidebar-header" defer>
-    <header class="flex h-header items-center gap-x-2 border-b px-6">
+    <header class="flex items-center gap-x-3 border-b px-6 py-2.5">
       <SidebarTrigger class="data-[open=true]:hidden" :data-open="open" />
-      <h1 class="text-sm">Detecções</h1>
-      <span
-        class="inline-flex aspect-[3.33/4] items-center justify-center rounded-sm bg-muted px-1 text-2xs font-medium"
-      >
-        5
-      </span>
+
+      <div class="relative items-center">
+        <Input type="text" placeholder="Encontrar" class="pl-9" />
+
+        <span
+          class="absolute inset-y-0 flex items-center justify-center px-2.5"
+        >
+          <Search class="size-4 text-muted-foreground" />
+        </span>
+      </div>
 
       <div
-        class="ml-auto flex items-center gap-1.5 text-xs text-muted-foreground [&>svg]:size-5"
+        class="ml-auto flex shrink-0 items-center gap-1.5 text-xs text-muted-foreground [&>svg]:size-5"
       >
         <template v-if="propostas.isFetching.value">
           <ListRestart />
           <span>Atualizando...</span>
         </template>
-        <template v-else-if="propostas.isSuccess.value">
+
+        <template
+          v-if="!propostas.isFetching.value && propostas.isSuccess.value"
+        >
           <ListCheck />
           <span>Tudo certo</span>
         </template>
